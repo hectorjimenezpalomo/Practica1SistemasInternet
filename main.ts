8390e1e11b74e9528a406af2f87c8601c69f40a1
Juan7334wqa <xue7334xi@gmail.com>
2025-10-01 14:58:24 +0200

    Paso2 listasLugares

--- main.ts
+++ main.ts
@@ -1,5 +1,52 @@
-console.log("hola mundo")
+type Lugar={
+    id:number,
+    nombre:String,
+    tipo:string,
+    peligro:number
+}
 
-console.log("QUE TAL QUANAN")
+type Personaje={
+    id:number,
+    nombre:String,
+    fuerza:number,
+    aliado:boolean
+}
 
-console.log("Mieeeeeeeeeeeeee")
+type Objeto={
+    id:number,
+    nombre:String,
+    poder:number,
+    categoria:String
+}
+
+const lugares: Lugar[] = [
+  { id: 1, nombre: "Bosque Misterioso", tipo: "bosque", peligro: 2 },
+  { id: 2, nombre: "Cueva Oscura", tipo: "cueva", peligro: 5 },
+  { id: 3, nombre: "Castillo Abandonado", tipo: "castillo", peligro: 4 }
+];
+ 
+const personajes: Personaje[] = [
+  { id: 1, nombre: "Guardián", fuerza: 8, aliado: false },
+  { id: 2, nombre: "Sabio", fuerza: 2, aliado: true },
+  { id: 3, nombre: "Bandido", fuerza: 6, aliado: false }
+];
+ 
+const objetos: Objeto[] = [
+  { id: 1, nombre: "Espada", poder: 5, categoria: "arma" },
+  { id: 2, nombre: "Antorcha", poder: 1, categoria: "herramienta" },
+  { id: 3, nombre: "Amuleto", poder: 3, categoria: "mágico" },
+  { id: 4, nombre: "Escudo", poder: 4, categoria: "arma" }
+];
+
+
+const listarLugares =(recorrer:Lugar[]): void =>{
+    recorrer.forEach((dato:Lugar)=>{
+        console.log(dato.nombre,"(peligro: ",dato.peligro," )")
+    })
+
+}
+
+console.log(listarLugares(lugares))
+
+
+
